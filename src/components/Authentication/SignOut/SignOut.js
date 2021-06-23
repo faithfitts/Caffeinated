704ea1ed31baec032cdcbd27b70732f1eef0b4de
faithfitts <faithfitts@gmail.com>
2021-06-23 10:28:37 -0700

    Correct Auth Paths for Alert Messages
    - correct alert message path for sign-up
    - correct alert message path for sign-in
    - correct alert message path for change-password
    - correct alert message path for sign-out

--- src/components/Authentication/SignOut/SignOut.js
+++ src/components/Authentication/SignOut/SignOut.js
@@ -2,7 +2,7 @@ import { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
 import { signOut } from '../../../api/auth'
-import messages from '../AutoDismissAlert/messages'
+import messages from '../../AutoDismissAlert/messages'
 
 class SignOut extends Component {
   componentDidMount () {
